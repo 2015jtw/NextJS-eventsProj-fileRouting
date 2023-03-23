dd730034a57cba672bfe05c444b89b32e6edf8ff
Josh <2015JTW@gmail.com>
2023-03-23 13:55:45 -0400

    feat: created react component for home page, added styling and icons

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,17 @@
+import {getFeaturedEvents} from '../data/dummy-data'
+import EventList from '../components/events/event-list';
 
 export default function FeaturedEventsPage(){
+
+    const featuredEvents = getFeaturedEvents();
 
     return(
         <div>
             <h1>Featured Events Page</h1>
 
-            <h3>Features Events</h3>
+            <EventList items={featuredEvents} />
 
-            <ul>
-                <li>Test Featured Event 1</li>
-                <li>Test Featured Event 2</li>
-                <li>Test Featured Event 3</li>
-            </ul>
+            
         </div>
     )
 }
